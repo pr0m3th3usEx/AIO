1d0a5e018a82a78e502b52ee204d8b01edcefebd
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-11-17 23:26:14 +0100

    feat: add navigation bar

---
+++ dash_front/src/components/NavBar.tsx
@@ -0,0 +1,87 @@
+import { Image } from '@chakra-ui/image';
+import { HStack, VStack, Text, Box } from '@chakra-ui/layout';
+import UnknownProfileImage from 'assets/UnknownProfileImage.jpg';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavigationMenu = (): JSX.Element => {
+  const logout = (): void => {
+    localStorage.clear();
+    window.location.href = '/auth/login';
+  };
+
+  return (
+    <VStack
+      position="absolute"
+      bottom="-10px"
+      bg="white"
+      right="0"
+      width="250px"
+      borderRadius="6px"
+      spacing={0}
+      transform="translate(0, 100%)"
+    >
+      <Box
+        w="100%"
+        align="center"
+        padding="18px 24px"
+        cursor="pointer"
+        _hover={{ opacity: '0.43' }}
+      >
+        <Link to="/dashboard/profile">
+          <Text fontSize="18px" color="black">
+            Profile / Services
+          </Text>
+        </Link>
+      </Box>
+      <Box bg="gray.light" h="1px" w="100%" />
+      <Box
+        w="100%"
+        align="center"
+        padding="18px 24px"
+        cursor="pointer"
+        _hover={{ opacity: '0.43' }}
+        onClick={logout}
+      >
+        <Text fontSize="18px" color="black">
+          Log out
+        </Text>
+      </Box>
+    </VStack>
+  );
+};
+
+const NavBar = (): JSX.Element => {
+  const [menuDisplay, setMenuDisplay] = useState<boolean>(false);
+
+  return (
+    <HStack
+      h="72px"
+      w="100%"
+      bg="tiffany"
+      px="32px"
+      py="12px"
+      justifyContent="space-between"
+    >
+      <Text fontWeight="bold" fontSize="24px">
+        Dashboard
+      </Text>
+      <VStack position="relative">
+        <Image
+          onClick={() => setMenuDisplay(!menuDisplay)}
+          cursor="pointer"
+          w="48px"
+          h="48px"
+          borderRadius="50%"
+          src={UnknownProfileImage}
+          {...(menuDisplay && {
+            boxShadow: `0px 0px 4px 4px rgba(240, 240, 240, 0.32)`,
+          })}
+        />
+        {menuDisplay && <NavigationMenu />}
+      </VStack>
+    </HStack>
+  );
+};
+
+export default NavBar;
--- dash_front/src/pages/dashboard/Layout.tsx
+++ dash_front/src/pages/dashboard/Layout.tsx
@@ -1,11 +1,17 @@
-import { Box } from '@chakra-ui/layout';
+import { VStack } from '@chakra-ui/layout';
+import NavBar from 'components/NavBar';
 
 const Layout = ({
   children,
 }: {
   children: JSX.Element[] | JSX.Element;
 }): JSX.Element => {
-  return <Box>{children}</Box>;
+  return (
+    <VStack w="100%" minH="100vh" bg="gray.light">
+      <NavBar />
+      {children}
+    </VStack>
+  );
 };
 
 export default Layout;

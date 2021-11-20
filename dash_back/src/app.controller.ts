57a552209046fdfe7175626b66aa842eb11eb8f8
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-11-20 02:08:31 +0100

    feat: add about.json

---
+++ dash_back/src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Request } from '@nestjs/common';
+import { ServiceConfiguration } from './services/services.dto';
+import AVAILABLE_SERVICES from './services/services.json';
+import { IpAddr } from './utils/ip.decorator';
+import { WidgetConfiguration } from './widget/widget.dto';
+
+type About = {
+  client: {
+    host: string;
+  };
+  server: {
+    current_time: number;
+    services: {
+      name: string;
+      widgets: WidgetConfiguration[];
+    }[];
+  };
+};
+
+@Controller('')
+export class AppController {
+  @Get('about.json')
+  async about(@Request() req, @IpAddr() addr: string): Promise<About> {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    return {
+      client: {
+        host: ip,
+      },
+      server: {
+        current_time: 0,
+        services: <ServiceConfiguration[]>AVAILABLE_SERVICES.map((service) => ({
+          name: service.name,
+          widgets: service.widgets,
+        })),
+      },
+    };
+  }
+}
--- dash_back/src/app.module.ts
+++ dash_back/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { ServiceModule } from './services/service.module';
 import { UserModule } from './user/user.module';
@@ -6,5 +7,6 @@ import { WidgetModule } from './widget/widget.module';
 
 @Module({
   imports: [AuthModule, UserModule, WidgetModule, ServiceModule],
+  controllers: [AppController],
 })
 export class AppModule {}

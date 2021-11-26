67f80a034d6c6974d9b3f20c91c41f3ae7999388
Pierre Dubosq <pierre@dubosq.eu>
2021-11-26 19:44:23 +0100

    Fetch module list

--- dash_back/src/apis/intra.ts
+++ dash_back/src/apis/intra.ts
@@ -20,9 +20,33 @@ export class Intra
         });
     }
 
+    list_module = async () => {
+        return await fetch(this.#autologin + "/module/board/?format=json&start=2021-01-01&end=2022-12-31").then(j => {
+            
+            return j.json().then(element => {
+                let modules = [];
+                element.forEach(element => {
+                    modules.push(element.codemodule);
+                });
+                return modules;
+            });
+        });
+    }
+
     module = async (module: string) => {
         return await fetch(this.#autologin + "/module/2021/" + module + "/BAR-5-1/?format=json").then(j => {
             return j.json();
         });
     }
 }
+
+/*
+const i = new Intra("https://intra.epitech.eu/auth-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+i.list_module().then(j => {
+    i.module(j[0]).then(j => {
+        console.log(j);
+    });
+}).catch(e => {
+    console.log(e);
+});
+*/
